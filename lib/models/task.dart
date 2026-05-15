eb2e5ef3cc35ed2517840a0c3baf66a72018c59f
Rimuru236 <levelsfashion39@gmail.com>
2026-05-15 02:25:39 -0700

    models files

---
+++ lib/models/task.dart
@@ -0,0 +1,21 @@
+class Task {
+  String title;
+  String description;
+  String category;  
+  String priority;  
+  DateTime dueDate;
+  bool isCompleted;
+
+  Task({
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.priority,
+    required this.dueDate,
+    this.isCompleted = false,
+  });
+
+ 
+  bool get isOverdue =>
+      !isCompleted && dueDate.isBefore(DateTime.now());
+}
